d012d5f567861dca060e2465753386700eb939d7
unknown <>
2016-07-19 13:51:48 +0800

    first

--- app/src/main/java/com/qkhl/mycustomerview/MainActivity.java
+++ app/src/main/java/com/qkhl/mycustomerview/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		//自定义控件1
+		//自定义控件12
     }
 }
